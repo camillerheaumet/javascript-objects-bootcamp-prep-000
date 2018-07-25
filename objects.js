75e8b382c158a195005fa939c9331b9ac9cbce6e
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-25 15:23:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {"artist name": "song title"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign(playlist, { artistName: songTitle })
+  Object.assign(playlist, {[artistName]: songTitle })
   return playlist
 }
 
