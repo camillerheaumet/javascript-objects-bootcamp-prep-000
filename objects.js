09167dc1fe4b743dd214338be378233953696f2e
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-25 15:11:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {"artist name": "song title"}
+var playlist = {"artist names": "song titles"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName = songTitle
