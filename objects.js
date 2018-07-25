9713111e8a9dcf03663b4ebb4b88dbecdaaf0a65
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-07-25 15:26:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(songTitle, artistName) {
-  delete playlist.songTitle ;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName ;
   return playlist
 }
